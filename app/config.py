8ff960d5fd3590627a29e42af87b30e5e536383d
Shanur <shanurislam@gmail.com>
2024-02-06 21:10:06 +0000

    added os variables

--- app/config.py
+++ app/config.py
@@ -1,12 +1,15 @@
 from datetime import timedelta
+import os
 
 class Config:
     """
     Configuration class for Flask application.
     """
     DEBUG = True  # Set to False in production
-    SECRET_KEY = '9335d4f958c8b6694703556933d82e389647ef15bcd78b3abe5d6acbf07f3ec7'
-    JWT_SECRET_KEY = '46d54bbbf94a9d201dcc4df7de307dcd8d8b71d7716a23f16e16b5cb4f971157'
+    # SECRET_KEY = '9335d4f958c8b6694703556933d82e389647ef15bcd78b3abe5d6acbf07f3ec7'
+    # JWT_SECRET_KEY = '46d54bbbf94a9d201dcc4df7de307dcd8d8b71d7716a23f16e16b5cb4f971157'
+    SECRET_KEY = os.environ(SECRET_KEY)
+    JWT_SECRET_KEY = os.environ(JWT_SECRET_KEY)
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
     JWT_TOKEN_LOCATION = 'cookies'
     JWT_COOKIE_SECURE = False  # Set to True in production (requires HTTPS)
@@ -14,11 +17,3 @@ class Config:
 
     # Set cookie expiration to match token expiration
     JWT_COOKIE_EXPIRES = timedelta(minutes=30)
-
-
-db_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': 'Perad303',
-    'database': 'testdb',
-}
--- app/main.py
+++ app/main.py
@@ -1,11 +1,21 @@
-import mysql.connector
+import os
+
+# connection = {
+#     'charset': "utf8mb4",
+#     'connect_timeout': 10,
+#     'database': "testdb",
+#     'host': "mysql-c84cbdd-shanurislam-api.a.aivencloud.com",
+#     'password': "AVNS_KfXrQqgF7G0Yp3FXD2Z",
+#     'port': 19082,
+#     'user': "avnadmin",
+# }
 
 connection = {
     'charset': "utf8mb4",
     'connect_timeout': 10,
-    'database': "testdb",
-    'host': "mysql-c84cbdd-shanurislam-api.a.aivencloud.com",
-    'password': "AVNS_KfXrQqgF7G0Yp3FXD2Z",
-    'port': 19082,
-    'user': "avnadmin",
+    'database': os.environ.get("MYSQL_DATABASE"),
+    'host': os.environ.get("MYSQL_HOST"),
+    'password': os.environ.get("MYSQL_PASSWORD"),
+    'port': int(os.environ.get("MYSQL_PORT", 3306)),  # Default to 3306 if not set
+    'user': os.environ.get("MYSQL_USER"),
 }
