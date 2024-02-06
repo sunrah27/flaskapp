7c1d5111c55926c2e5a96c57349ef84e3f0904db
Shanur <shanurislam@gmail.com>
2024-02-06 21:00:06 +0000

    - switched to Aiven hosted database - fixed code to make login/registration work

--- app/db_operations.py
+++ app/db_operations.py
@@ -1,6 +1,7 @@
 # app/db_operations.py
 import mysql.connector
-from app.config import db_config
+# from app.config import db_config
+from app.main import connection
 from app.constants import UserCodes
 
 class DatabaseConnectionError(Exception):
@@ -11,7 +12,8 @@ class DatabaseConnectionError(Exception):
 def get_db_connection():
     try:
         # Use context manager for database connection
-        return mysql.connector.connect(**db_config)
+        # return mysql.connector.connect(**db_config)
+        return mysql.connector.connect(**connection)
 
     except mysql.connector.Error as err:
         print(f"Error: {err}")
---
+++ app/main.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+connection = {
+    'charset': "utf8mb4",
+    'connect_timeout': 10,
+    'database': "testdb",
+    'host': "mysql-c84cbdd-shanurislam-api.a.aivencloud.com",
+    'password': "AVNS_KfXrQqgF7G0Yp3FXD2Z",
+    'port': 19082,
+    'user': "avnadmin",
+}
