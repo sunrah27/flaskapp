1e5170271e56ea204cbc3e071db09996dd425409
Shanur <shanurislam@gmail.com>
2024-02-12 17:28:32 +0000

    cookie nightmare 4

--- app/user_routes.py
+++ app/user_routes.py
@@ -4,7 +4,6 @@ from flask import Blueprint, request, jsonify, make_response, send_file, current
 from flask_jwt_extended import jwt_required, get_jwt_identity, create_access_token
 from app.db_operations import get_db_connection
 from app.constants import UserCodes
-from app.config import DOMAIN
 import hashlib
 import mysql.connector
 import secrets
@@ -116,7 +115,7 @@ def login_user():
                 fullname = f"{fname} {lname}"
                 access_token = create_access_token(identity={'user_id': user_id,'fullname': fullname})
                 response = make_response(jsonify({"message": "Login successful"}), 200)
-                response.headers.set('Set-Cookie', f'access_token_cookie={access_token}; Domain=https://sunrah27.github.io/eCommerce/; Secure; HttpOnly; Path=/')
+                response.headers.set('Set-Cookie', f'access_token_cookie={access_token}; Domain=.github.io; Secure; HttpOnly; Path=/eCommerce')
                 logger.info("Login successful: %s: %s", user_id, email)
                 return response
             else:
